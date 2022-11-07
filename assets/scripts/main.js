e9deec01ce2620c4e0d4988179ea2709c8f6104d
Sanjit Joseph <smjoseph@ucsd.edu>
2022-11-06 23:49:18 -0800

    Implement getRecipesFromStorage function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return JSON.parse(window.localStorage.getItem('recipes'));
 }
 
 /**
