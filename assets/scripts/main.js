2396f3cb9739b59fe618736f068fe3bad833e034
Sanjit Joseph <smjoseph@ucsd.edu>
2022-11-07 15:18:52 -0800

    Implement saveRecipesToStorage function - B1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -59,6 +59,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
