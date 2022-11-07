9cf20ce0fa6dd95a76a13db78a0d614b547e2360
Sanjit Joseph <smjoseph@ucsd.edu>
2022-11-06 22:55:08 -0800

    Attach shadow DOM to RecipeCard

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,6 +8,8 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    let shadowEl = this.attachShadow({mode:'open'});
+    
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
